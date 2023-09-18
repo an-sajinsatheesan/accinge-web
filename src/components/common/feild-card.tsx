08c4c16081bbd9712304c7fe939a5d9daf9d5564
an.sajinsatheesan <an.sajinsatheesan@gmail.com>
2023-09-18 14:42:57 +0530

    CommonCardRedo

--- src/components/common/feild-card.tsx
+++ src/components/common/feild-card.tsx
@@ -2,10 +2,10 @@ import Image from "next/image"
 
 
 
-const FeildCard = ({ img, mainTitle, subTitle }: Feilds) => {
+const FeildCard = ({ col, img, mainTitle, subTitle }: Feilds) => {
     return <>
-        <div className="bg-[url('./../../public/static/assets/beams-pricing.png')] bg-no-repeat bg-center bg-cover rounded-3xl">
-            <div className='rounded-3xl text-center border border-gray-100 p-3 shadow-2xl shadow-gray-600/10 sm:col-span-2 sm:px-12 lg:col-span-1 lg:row-span-2'>
+        <div className="">
+            <div className={`${col} rounded-3xl text-center p-3 shadow-2xl shadow-gray-600/10 sm:col-span-2 sm:px-12 lg:col-span-1 lg:row-span-2`}>
                 <div className='items-center flex w-100 justify-center'>
                     <Image src={img} alt="ITC logo" width={40} height={40} className='fill-[#FF9800]' />
                 </div>
--- src/pages/home/landing-company/company.tsx
+++ src/pages/home/landing-company/company.tsx
@@ -10,20 +10,28 @@ const Company: React.FC = () => {
     return (
         <>
             {/* <Image src="/static/assets/overlayMd.webp" alt="" width={1580} height={300} className='w-100' layout="responsive"/> */}
-            <div className="bg-[url('./../../public/static/assets/beams-pricing.png')] bg-no-repeat bg-top bg-cover ">
-                <div className="flex flex-col w-full items-center justify-around md:flex-row py-16 px-20 gap-2">
+            <div className="rounded-lg bg-[#f9e3f8] max-w-6xl mx-auto mt-20" >
+                <div className="flex flex-col w-full items-center justify-around md:flex-row py-12 px-3 gap-2">
                     <div className="max-w-lg flex flex-col">
-                        <h1 className='w-100 grid justify-start font-semibold text-[13px] text-[#FF9800] mb-3 tracking-[1.5px] leading-6'>OUR COMPANY</h1>
-                        <h1 className='text-[23px] tracking-[1px] leading-8'>Accinge provides software solutions as well as bespoke software development
+                        <h1 className='w-100 grid justify-start font-semibold text-[13px]  mb-3 tracking-[1.5px] leading-4 text-[#fe6f46]'>OUR COMPANY</h1>
+                        <h1 className='text-[17px] tracking-[1px] leading-8'>Accinge provides software solutions as well as bespoke software development
                             services for corporate establishments and government sectors in UAE and EMEA region.</h1>
                     </div>
-                    <div className="w-full sm:w-2/4 md:w-1/3"> <h1 className='text-gray-600 leading-[22px] mt-3'>
+                    <div className="w-full sm:w-2/4 md:w-1/3"> <h1 className='text-gray-600 leading-7 mt-3 italic'>
                         Accinge is a product engineering services company focused on high quality AGILE software product development starting from software blueprinting and
                         wireframing to deployments at scale.
                         We partner with global enterprises, helping them to apply innovation in every facet of business.</h1>
                     </div>
                 </div>
-                <h1 className="w-100 text-center pb-6 cursor-pointer text-teal-950">Your search for the right innovation partner ends here. <Link href="about"><span className='text-teal-950 font-extrabold'> Find out more about Us </span></Link></h1>
+                <div className="flex w-100 pb-8 items-center justify-center gap-2">
+                    <h1 className="text-center cursor-pointer text-teal-950">Your search for the right innovation partner ends here.</h1>
+                    <Link href="about">
+                        <div className="flex items-center justify-center gap-2">
+                            <span className='text-[#fe6f46] font-extrabold'> Find out more about Us </span>
+                            <Image src="/static/assets/arrow.svg" alt="Arrow" width={25} height={25} className='animate-ping arrow-col' />
+                        </div>
+                    </Link>
+                </div>
             </div>
 
         </>
